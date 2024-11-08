e3cae9ddb2df4d198d3d090a1849c36a74d57d9e
piyushkurzekar <piyushkurzekar8390@gmail.com>
2024-11-08 18:19:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 1300,
+        loop:true,
+        
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
+
+
+
+  
